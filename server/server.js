910209623e0c4e6d0bb918c4f916df013a64ea03
Ajasa Taiwo <ajasataiwo45@gmail.com>
2024-03-19 07:13:22 +0100

    initial commit

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // Routes
 app.use('/api/home', somethingRoutes);
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.get('/', (req, res) => res.status(200).json({message: "Welcome Back Taiwo"});
 
 app.get('/about', (req, res) => res.send('About Page Route'));
 
